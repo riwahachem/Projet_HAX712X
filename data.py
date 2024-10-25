68874122051661fb4babc348f927fd611c87c432
LucienDuigou <luluduipou@hotmail.fr>
2024-10-25 11:15:56 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,13 @@
 
-filename = 'TAM_MMM_CoursesVelomagg_2022.csv'
+#importation des données csv des courses velomagg durant l'année 2021
+url="https://data.montpellier3m.fr/sites/defaukt/files/ressources/TAM_MMM_CoursesVelomagg_2021.csv"
+path_target="../data/CoursesVelomagg_2021.csv"
+path, fname = os.path.split(path_target)
+pooch.retrieve(url, path=path, fname=fname, known_hash=None)
+df_CoursesVelomagg_2021=pd.read_csv("../data/CoursesVelomagg_2021.csv")
+df_CoursesVelomagg_traité=df_CoursesVelomagg_2021.dropna()
+
+filename = 'TAM_MMM_CoursesVelomagg_2021.csv'
 
 def data(date, heure, depart, arrivee) :
   L=[]
