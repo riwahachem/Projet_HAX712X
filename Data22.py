7bbeef75aa12616087513d18ffe77f98ab4e4e4a
e20240001069 <lamia.oulebsir@etu.umontpellier.fr>
2024-10-29 17:56:48 +0100

    modif

--- Data22.py
+++ Data22.py
@@ -16,11 +16,12 @@ df_2022.dropna(subset=['Covered distance (m)', 'Duration (sec.)', 'Departure', '
 statistiques_descriptives = df_2022[['Covered distance (m)', 'Duration (sec.)']].describe()
 print("Statistiques descriptives pour l'année 2021 :")
 print(statistiques_descriptives)
+display(statistiques_descriptives)  # Utiliser display pour afficher les résultats
 
 # Visualisation avec un swarm plot
 print("figure ")
 plt.figure(figsize=(12, 6))
-sns.swarmplot(x='Covered distance (m)', y='Duration (sec.)', data=df_2022, alpha=0.7)
+sns.stripplot(x='Covered distance (m)', y='Duration (sec.)', data=df_2022.sample(500), alpha=0.7)
 plt.title('Swarm Plot de la Durée des Trajets par Distance Parcourue (2022)')
 plt.xlabel('Distance Parcourue (m)')
 plt.ylabel('Durée (sec.)')
