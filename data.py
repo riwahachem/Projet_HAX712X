68f5887cd2188647716b720cff05119fd9ec126f
LucienDuigou <luluduipou@hotmail.fr>
2024-11-01 22:29:14 +0100

    Update data.py

--- data.py
+++ data.py
@@ -22,3 +22,39 @@ def data(date, heure, depart, arrivee) :
             if x[6].split(" ")[1,:] == arrivee.split(" "):
               L.append([x[5],x[6]])
   return L
+
+def trajets_jour(j,m,a):
+    if j<10:
+        jj='0'+str(j)
+    else :
+        jj=str(j)
+    if m<10:
+        mm='0'+str(m)
+    else :
+        mm=str(m)
+    aaaa=str(a)
+    L=[]
+    with open(filename) as f:
+        for ligne in f :
+            if ligne.split(",")[2]!='Deperture':
+                x=ligne.split(",")
+                if x[2].split('-')[0]==aaaa:
+                    if x[2].split('-')[1]==mm:
+                        if x[2].split('-')[2].split(' ')[0]==jj:
+                            L.append([x[5],x[6]])
+    M=[]
+    for i in L:
+        v=0
+        c=0
+        n=0
+        for j in L:
+            if j==i:
+                c+=1
+        for k in M:
+            if i==[k[0],k[1]]:
+                n=1
+        if n==0:
+            i.append(c)
+            M.append(i)
+            v+=1
+    return M
