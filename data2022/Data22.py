fc7f7d71368f566cb635f14c004eb24e1f101718
LamiaOulebsir <lamia.oulebsir@etu.umontpellier.fr>
2024-11-01 15:16:30 +0100

    Update Data22.py

--- data2022/Data22.py
+++ data2022/Data22.py
@@ -3,26 +3,26 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-# Charger le fichier de données de 2022
-chemin_fichier =  r'C:\Users\Clara\Desktop\dossier Projet\TAM_MMM_CoursesVelomagg_2022.csv' 
-df_2022 = pd.read_csv(chemin_fichier)
+# Charger le fichier de données 
+chemin_fichier =  r'C:\Users\Clara\Desktop\dossier Projet\TAM_MMM_CoursesVelomagg.csv' 
+df = pd.read_csv(chemin_fichier)
 
 # Nettoyer et convertir les dates
-df_2022['Departure'] = pd.to_datetime(df_2022['Departure'], errors='coerce')
-df_2022['Return'] = pd.to_datetime(df_2022['Return'], errors='coerce')
-df_2022.dropna(subset=['Covered distance (m)', 'Duration (sec.)', 'Departure', 'Return'], inplace=True)
+df['Departure'] = pd.to_datetime(df['Departure'], errors='coerce')
+df['Return'] = pd.to_datetime(df['Return'], errors='coerce')
+df.dropna(subset=['Covered distance (m)', 'Duration (sec.)', 'Departure', 'Return'], inplace=True)
 
 # Calculer les statistiques descriptives
-statistiques_descriptives = df_2022[['Covered distance (m)', 'Duration (sec.)']].describe()
-print("Statistiques descriptives pour l'année 2021 :")
+statistiques_descriptives = df[['Covered distance (m)', 'Duration (sec.)']].describe()
+print("Statistiques descriptives :")
 print(statistiques_descriptives)
 display(statistiques_descriptives)  # Utiliser display pour afficher les résultats
 
 # Visualisation avec un swarm plot
 print("figure ")
 plt.figure(figsize=(12, 6))
-sns.stripplot(x='Covered distance (m)', y='Duration (sec.)', data=df_2022.sample(500), alpha=0.7)
-plt.title('Swarm Plot de la Durée des Trajets par Distance Parcourue (2022)')
+sns.stripplot(x='Covered distance (m)', y='Duration (sec.)', data=df.sample(500), alpha=0.7)
+plt.title('Swarm Plot de la Durée des Trajets par Distance Parcourue')
 plt.xlabel('Distance Parcourue (m)')
 plt.ylabel('Durée (sec.)')
 plt.grid(True)
