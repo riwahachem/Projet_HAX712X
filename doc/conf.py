ef5b9117eb0f684b07f349259e46499631e8aa97
Riwa Hachem <riwahachemreda@gmail.com>
2024-11-04 21:25:57 +0100

    update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -29,7 +29,7 @@ import sys
 sys.path.insert(0, os.path.abspath('../atvm'))
 
 
-html_theme = 'bizstyle'
+html_theme = 'furo'
 html_static_path = ['_static']
 #import sphinx_rtd_theme
 #html_theme = 'sphinx_rtd_theme'
