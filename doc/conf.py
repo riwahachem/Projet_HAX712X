022695e84bb7fdbb0dec9bbcbaddf51b8958c479
Riwa Hachem Reda <riwahachemreda@gmail.com>
2024-11-04 13:46:24 +0100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -29,5 +29,12 @@ import sys
 sys.path.insert(0, os.path.abspath('../atvm'))
 
 
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+#html_static_path = ['_static']
+import sphinx_rtd_theme
+
+html_theme = 'sphinx_rtd_theme'
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 html_static_path = ['_static']
