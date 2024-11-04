0257c951e1d6d18ef089d960121007c206b042a7
Riwa Hachem <riwahachemreda@gmail.com>
2024-11-04 11:44:14 +0100

    update __init__.py file

---
+++ atvm/io/__init__.py
@@ -0,0 +1,5 @@
+import os 
+url_courses_24 ="https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_CoursesVelomagg.csv"
+path_target= os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),"..","data"
+)
