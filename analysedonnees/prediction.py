2f75ded3a83ba5cdfb20250b1f2bf5e5a611b008
LucienDuigou <luluduipou@hotmail.fr>
2024-11-19 20:51:29 +0100

    Delete analysedonnees/prediction.py

--- analysedonnees/prediction.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-filename = 'TAM_MMM_CoursesVelomagg_2022.csv'
-
-def prediction_jour(j,m,a):
-    M,l=semaine_trajet[(determination_jour(j,m,a)%7)-1][0],semaine_trajet[(determination_jour(j,m,a)%7)-1][1]
-    for i in range(len(M)):
-        M[i][2]=[M[i][2]-(1.96*(np.sqrt((M[i][2]*(1-M[i][2]))/l))),M[i][2]+(1.96*(np.sqrt((M[i][2]*(1-M[i][2]))/l)))]
-    return M 
