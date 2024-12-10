76206c19fefd8b417f98af5a051cf2aa74d36e9d
Riwa Hachem Reda <riwahachemreda@gmail.com>
2024-12-10 18:42:45 +0100

    cosmit

--- atvm/preprocess/__init__.py
+++ atvm/preprocess/__init__.py
@@ -1,6 +1,3 @@
-
-
 from .coord_arrets import get_station_coordinates
 from .trajets_jour import trajets_analys
 from .utils import corriger_encodage
-from ...code.donnees_EcoCompt.prediction import determination_jour,  données_jour_semaine, stats_jour_semaine, intensity_jour_stats, stat_heure_jour, poids_heure, route_prediction_jour, couleur_intensity, couleur_intensity_heure, carte_prediction_jour, carte_prediction_jour_heure
